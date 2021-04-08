242c3da3e9986b0c921b58eab45857888eada5d0
Mradul Jain <mradul.jain90@gmail.com>
2021-04-08 11:43:29 +0530

    Stripe payment integration

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+$config = array(
+    "name" => "Stripe Payment Gateway",
+    "description" => "This extension is devised as the channel to make payments. The procedure to make payments includes the customer requiring to fill in some details, like credit/debit card number, expiry date, and CVV.",
+    "is_default_active" => 1,
+    "version" => "1.0.0",
+    "image_name" => "pay-per-click.png",
+    "setting_link" => "settings/integrations/stripe_payment_gateway",
+    "gateway_key" => "stripe"
+);
