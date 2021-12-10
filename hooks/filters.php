0f9adb6344ee0f982b279b4325491924acff1bf3
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2021-12-10 12:11:39 +0530

    Stripe with channexPCI fixes

--- hooks/filters.php
+++ hooks/filters.php
@@ -9,7 +9,7 @@ function stripe_tokenize($data) {
     unset($filtered_data['customer_data']);
 
     $CI = &get_instance();
-    
+
     $CI->load->library('../extensions/stripe-integration/libraries/ProcessPayment');
     $token_response = $CI->processpayment->create_token($filtered_data['card']['service_code'],
      $filtered_data['card']['card_number'], $filtered_data['card']['expiration_month'], $filtered_data['card']['expiration_year']);
@@ -31,7 +31,17 @@ function stripe_tokenize($data) {
             $card_details['cc_cvc_encrypted'] = null;
             $card_details['cc_number'] = 'XXXX XXXX XXXX ' . substr($filtered_data['card']['card_number'], -4);
             $meta['customer_id'] = $response['customer_id'];
-            $meta['token'] = $token_response['token'];
+            if(
+                isset($filtered_data['tokenization_response']) &&
+                isset($filtered_data['tokenization_response']['data']) &&
+                isset($filtered_data['tokenization_response']['data']['attributes']) &&
+                isset($filtered_data['tokenization_response']['data']['attributes']['card_token']) &&
+                $filtered_data['tokenization_response']['data']['attributes']['card_token']
+            ){
+                $meta['token'] = $filtered_data['tokenization_response']['data']['attributes']['card_token'];
+            } else {
+                $meta['token'] = $token_response['token'];
+            }
             $card_details['customer_meta_data'] = json_encode($meta);
             $CI->Card_model->create_customer_card_info($card_details);
         }
