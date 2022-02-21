c5cced0b99cc23ecb16b7ab7911fdf42d43dd9ca
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-02-21 16:10:10 +0530

    set stripe as supported in minimal subscription extension

--- config/config.php
+++ config/config.php
@@ -9,6 +9,6 @@ $config = array(
     "setting_link" => "stripe_payment_gateway",
     "gateway_key" => "stripe",
     "categories" => array("payment_process"),
-    "is_open_source" => true,
+    "supported_in_minimal" => true,
     "marketplace_product_link" => "https://marketplace.minical.io/product/stripe-payment-gateway"
 );
